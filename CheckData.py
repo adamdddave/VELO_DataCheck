735fb1f55bd4e2d4ef4f1677dbc7122a6ac0c91e
Adam Davis <adamdddave@googlemail.com>
2019-07-29 16:24:50 +0100

    Make dataframes from csv implemented

--- CheckData.py
+++ CheckData.py
@@ -2,11 +2,36 @@
 import sys,os
 import pandas as pd
 
-if __name__=='main':
-    if len(sys.argv)<1:
-        print(please pass a file to parse and quickly create plots)
+def make_dataframes(infile):
+    datasets = {'VP0_Asic0':[],'VP0_Asic1':[],'VP0_Asic2':[],
+                'VP1_Asic0':[],'VP1_Asic1':[],'VP1_Asic2':[],
+                'VP2_Asic0':[],'VP2_Asic1':[],'VP2_Asic2':[],
+                'VP3_Asic0':[],'VP3_Asic1':[],'VP3_Asic2':[]
+    }
+    timestamp=[]
+    temp = []
+    name = []
+    voltage=[]
+    offset=[]
+    for line in infile:
+        ts,n,v,t,o = line.split(',')
+        o = float(o.strip(" \n"))
+        v = float(v)
+        t = float(t)
+        #print (ts,n,v,t,o)
+        n=n.strip()
+        date,ts = ts.split("T")
+        datasets[n].append([ts,v,t,o])
+        #sys.exit()
+    dfs = {}
+    for dsk,ds in datasets.items():
+        dfs[dsk]=pd.DataFrame(ds, columns=['Time','Voltage','Temp','Offset'])
+        print (dfs[dsk].head())
+
+
+if __name__ == "__main__":
+    if len(sys.argv)<2:
+        print('please pass a file to parse and quickly create plots')
         sys.exit()
-    # create dataset from input file
-    df = pd.read_csv(sys.argv[1],names='Timestamp, Name, Voltage, Temperature, Offset',header=None) 
-    #split dataframes by name
-    VPA = {}
+    dfs = make_dataframes(open(sys.argv[1]))
+    
